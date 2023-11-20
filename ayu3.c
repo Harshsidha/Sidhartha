a13cc597c5201f1dc0295af7f24ad2fbfd6f0c50
Harshsidha <144160490+Harshsidha@users.noreply.github.com>
2023-11-20 12:06:37 +0530

    Add files via upload

---
+++ ayu3.c
@@ -0,0 +1,10 @@
+// program to calculate simple intrest 
+#include 
+main ()
+{float pa, roi, si;
+printf(enter principle amount, rate of intrest and time)
+scanf("%f%f%f" &pa,&roi,&time);
+si= (pa*roi*time*)/100;
+printf("\n calculate simple intrest for principle amount %f rate of intrest %f time %f is %f", pa, roi, time, si,);
+
+}
